fa6c8f76a97b71b37bba59de953eb57b9799319f
Moe Lwin Hein <hein.moelwin@gmail.com>
2025-04-06 21:22:18 +0800

    Add c++ template

---
+++ template/main.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+#if fileio
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    string s;
+    getline(cin, s);
+
+    cout << s << endl;
+
+    return 0;
+}
